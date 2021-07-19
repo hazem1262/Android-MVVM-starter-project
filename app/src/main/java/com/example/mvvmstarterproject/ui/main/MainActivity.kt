3e1f5727d0dfa71d4213a8e4ab1afec0464671cb
hazem <hazem.ashraf126@gmail.com>
2021-07-19 14:26:49 +0200

    [add] use navigation component instead of old way of navigation

--- app/src/main/java/com/example/mvvmstarterproject/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/mvvmstarterproject/ui/main/MainActivity.kt
@@ -1,35 +1,19 @@
 package com.example.mvvmstarterproject.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import com.example.mvvmstarterproject.R
 import com.example.mvvmstarterproject.base.BaseActivity
-import com.example.mvvmstarterproject.ui.main.users.UsersFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<MainViewModel>() {
-
-    @Inject
-    lateinit var testDagger:String
 
     override val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel.printTestString()
-        Log.d("test", viewModel.toString())
-        test.text = testDagger
-        initFragment()
-    }
-
-    private fun initFragment() {
-        val testFragmentA = UsersFragment.newInstance()
-        supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, testFragmentA).commit()
     }
 
 }
--- app/src/main/java/com/example/mvvmstarterproject/ui/main/users/UsersFragment.kt
+++ app/src/main/java/com/example/mvvmstarterproject/ui/main/users/UsersFragment.kt
@@ -6,32 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import com.example.mvvmstarterproject.R
 import com.example.mvvmstarterproject.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.test_fragment_a.*
+import kotlinx.android.synthetic.main.users_fragment.*
 
 @AndroidEntryPoint
 class UsersFragment : BaseFragment<UsersViewModel>() {
 
     override val viewModel: UsersViewModel by viewModels()
-    companion object {
-        fun newInstance() = UsersFragment()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.test_fragment_a, container, false)
+        return inflater.inflate(R.layout.users_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         test.text = viewModel.testString
         viewModel.getUserDetails()
-        viewModel.userLiveData.observe(viewLifecycleOwner, Observer {
+        viewModel.userLiveData.observe(viewLifecycleOwner, {
             if (it != null){
                 Toast.makeText(activity, it.toString(), Toast.LENGTH_SHORT).show()
             }
