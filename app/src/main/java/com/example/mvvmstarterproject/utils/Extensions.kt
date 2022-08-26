b69a2ec5385dd610c5aaaf990d304c9da868540d
hazem <hazem.ashraf126@gmail.com>
2022-08-26 22:34:54 +0200

    add onDebouncedListener

--- app/src/main/java/com/example/mvvmstarterproject/utils/Extensions.kt
+++ app/src/main/java/com/example/mvvmstarterproject/utils/Extensions.kt
@@ -1,2 +1,17 @@
 package com.example.mvvmstarterproject.utils
 
+import android.view.View
+
+inline fun View.onDebouncedListener(
+    delayInClick: Long = 500L,
+    crossinline listener: (View) -> Unit
+) {
+    val enableAgain = Runnable { isEnabled = true }
+    setOnClickListener {
+        if (isEnabled) {
+            isEnabled = false
+            postDelayed(enableAgain, delayInClick)
+            listener(it)
+        }
+    }
+}
